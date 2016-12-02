115d88133a13c2c0b8bfcb73bb77da562a05422e
William Lyons <wlyons@gmail.com>
2016-12-02 13:25:07 -0800

    Initial Commit

---
+++ GSL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSL
+//
+//  Created by William Lyons on 2016-12-02.
+//  Copyright © 2016 William Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GSL
+//
+//  Created by William Lyons on 2016-12-02.
+//  Copyright © 2016 William Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
